ab5862b03118d2c5cf077755cdb4606f2ce8aad4
nesh2391 <nesh2391@gmail.com>
2021-02-10 14:01:09 -0600

    added initial card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { StoriesComponent } from "./stories/stories.component";
 import { BacklogComponent } from "./backlog/backlog.component";
 import { HomeComponent } from "./home/home.component";
 import { AccountComponent } from "./account/account.component";
+import { ProcessesComponent } from "./processes/processes.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "", component: HomeComponent },
+  { path: "process", component: ProcessesComponent },
   { path: "backlog", component: BacklogComponent },
   { path: "account", component: AccountComponent }
 ];
