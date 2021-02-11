2f052cb4f8db71751f9b027287f1a1857a598c9a
nesh2391 <nesh2391@gmail.com>
2021-02-11 14:50:10 -0600

    stabalizing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { BacklogComponent } from "./backlog/backlog.component";
 import { HomeComponent } from "./home/home.component";
 import { AccountComponent } from "./account/account.component";
 import { ProcessesComponent } from "./processes/processes.component";
+import { ReleasesComponent } from "./releases/releases.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "", component: HomeComponent },
   { path: "process", component: ProcessesComponent },
   { path: "backlog", component: BacklogComponent },
+  { path: "release", component: ReleasesComponent },
   { path: "account", component: AccountComponent }
 ];
 
