a76bcfb36fb54e9229e9d5ee19239440bfdd4c34
nesh2391 <nesh2391@gmail.com>
2021-02-16 16:00:44 -0600

    learnt: app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,9 @@ import { AccountComponent } from "./account/account.component";
 import { ProcessesComponent } from "./processes/processes.component";
 import { ReleasesComponent } from "./releases/releases.component";
 import { SprintComponent } from "./sprint/sprint.component";
+import { GuideexplanationComponent } from "./guide/guideexplanation/guideexplanation.component";
+import { StoriesexplanationComponent } from "./guide/storiesexplanation/storiesexplanation.component";
+import { ProcessexplanationComponent } from "./guide/processexplanation/processexplanation.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
@@ -18,7 +21,15 @@ const routes: Routes = [
   { path: "release", component: ReleasesComponent },
   { path: "sprint", component: SprintComponent },
   { path: "account", component: AccountComponent },
-  { path: "guide", loadChildren: "./guide/guide.module.ts" }
+  {
+    path: "guide",
+    component: GuideexplanationComponent,
+    children: [
+      { path: "stories", component: StoriesexplanationComponent },
+      { path: "processes", component: ProcessexplanationComponent }
+    ]
+  }
+  //{ path: "guide", loadChildren: "./guide/guide.module.ts" }
 ];
 
 @NgModule({
