ecd70e7171989f4de0374ff47fb4062b0425d883
nesh2391 <nesh2391@gmail.com>
2021-02-26 17:26:25 -0600

    1 file modified, 1 added

--- src/app/releases/releases.component.ts
+++ src/app/releases/releases.component.ts
@@ -1,15 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ReleasesObject } from "../general-interfaces/releases-object";
 
 @Component({
-  selector: 'app-releases',
-  templateUrl: './releases.component.html',
-  styleUrls: ['./releases.component.css']
+  selector: "app-releases",
+  templateUrl: "./releases.component.html",
+  styleUrls: ["./releases.component.css"]
 })
 export class ReleasesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
+  sampleDate: number = new Date().getDate();
+  mockData: ReleasesObject[] = [
+    {
+      releaseId: 1,
+      releaseName: "riest rlease",
+      releaseStartDate: new Date(),
+      releaseEndDate: new Date(),
+      sprints: [
+        {
+          sprintId: 1,
+          sprintName: "sample Test 1",
+          sprintStart: this.sampleDate,
+          sprintEnd: this.sampleDate,
+          stories: [
+            {
+              name: "story-test-1",
+              componentName: "feature-x",
+              state: 1,
+              sprintId: 1,
+              assignedTo: "john doe",
+              ticketChecked: false,
+              ticketDetails: null,
+              title: null,
+              nonDevelopmentStory: null,
+              featureName: null
+            },
+            {
+              name: "story-test-1",
+              componentName: "feature-x",
+              state: 1,
+              sprintId: 1,
+              assignedTo: "john doe",
+              ticketChecked: false,
+              ticketDetails: null,
+              title: null,
+              nonDevelopmentStory: null,
+              featureName: null
+            },
+            {
+              name: "story-test-1",
+              componentName: "feature-x",
+              state: 1,
+              sprintId: 1,
+              assignedTo: "john doe",
+              ticketChecked: false,
+              ticketDetails: null,
+              title: null,
+              nonDevelopmentStory: null,
+              featureName: null
+            }
+          ]
+        },
+        {
+          sprintId: 1,
+          sprintName: "sample Test 1",
+          sprintStart: this.sampleDate,
+          sprintEnd: this.sampleDate,
+          stories: [
+            {
+              name: "story-test-1",
+              componentName: "feature-x",
+              state: 1,
+              sprintId: 1,
+              assignedTo: "john doe",
+              ticketChecked: false,
+              ticketDetails: null,
+              title: null,
+              nonDevelopmentStory: null,
+              featureName: null
+            }
+          ]
+        }
+      ]
+    }
+  ];
 
-  ngOnInit() {
-  }
 
+
+
+
+
+
+  
 }
