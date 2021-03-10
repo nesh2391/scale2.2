304ee283a4d1c39c982406e0398f34d97ad42842
nesh2391 <nesh2391@gmail.com>
2021-03-10 14:28:08 -0600

    2 files modified

--- src/app/defect/defect.component.ts
+++ src/app/defect/defect.component.ts
@@ -5,6 +5,7 @@ import {
   MAT_DIALOG_DATA
 } from "@angular/material/dialog";
 import { DefectsObject } from "../general-interfaces/defects-object";
+import { QuillModule } from "ngx-quill";
 
 @Component({
   selector: "app-defect",
@@ -12,6 +13,9 @@ import { DefectsObject } from "../general-interfaces/defects-object";
   styleUrls: ["./defect.component.css"]
 })
 export class DefectComponent implements OnInit {
+      quillEditorRef;
+  pannelSelect: number = 1;
+  maxUploadFileSize = 1000000;
   constructor(
     public dialogRef: MatDialogRef<DefectsObject>,
     @Inject(MAT_DIALOG_DATA) public defect: DefectsObject
@@ -22,4 +26,42 @@ export class DefectComponent implements OnInit {
   onNoClick(): void {
     this.dialogRef.close();
   }
+
+    getEditorInstance(editorInstance: any) {
+    this.quillEditorRef = editorInstance;
+
+    //Printing here
+    console.log(this.quillEditorRef);
+    const toolbar = editorInstance.getModule("toolbar");
+    toolbar.addHandler("image", this.imageHandler);
+  }
+
+    imageHandler = (image, callback) => {
+    const input = <HTMLInputElement>document.getElementById("fileInputField");
+    document.getElementById("fileInputField").onchange = () => {
+      let file: File;
+      file = input.files[0];
+      // file type is only image.
+      if (/^image\//.test(file.type)) {
+        if (file.size > this.maxUploadFileSize) {
+          alert("Image needs to be less than 1MB");
+        } else {
+          const reader = new FileReader();
+          reader.onload = () => {
+            const range = this.quillEditorRef.getSelection();
+            const img = '<img src="' + reader.result + '" />';
+            this.quillEditorRef.clipboard.dangerouslyPasteHTML(
+              range.index,
+              img
+            );
+          };
+          reader.readAsDataURL(file);
+        }
+      } else {
+        alert("You could only upload images.");
+      }
+    };
+
+    input.click();
+  };
 }
