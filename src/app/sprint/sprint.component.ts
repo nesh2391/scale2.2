41784b36da9abf70ff9028e6961c334bcd8c21b7
nesh2391 <nesh2391@gmail.com>
2021-03-17 17:58:01 -0500

    2 files modified

--- src/app/sprint/sprint.component.ts
+++ src/app/sprint/sprint.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { SprintService } from "./sprint.service";
 
 @Component({
   selector: "app-sprint",
@@ -6,7 +7,11 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./sprint.component.css"]
 })
 export class SprintComponent implements OnInit {
-  constructor() {}
-
+  constructor(sprintService: SprintService) {}
+  sprintName: string = "";
   ngOnInit() {}
+
+  addSprint() {
+    this.sprintService.putNewSprintInList(this.sprintName);
+  }
 }
