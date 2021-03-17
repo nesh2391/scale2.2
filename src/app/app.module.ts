d00db43c537c3af30b97f4917db4385c1a55ae53
nesh2391 <nesh2391@gmail.com>
2021-03-17 12:53:46 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { SprintTableComponent } from "./sprint-table/sprint-table.component";
 import { ReleasesTableComponent } from "./releases-table/releases-table.component";
 import { DefectComponent } from "./defect/defect.component";
 import { SearchSelectComponent } from "./search-select/search-select.component";
+import { SprintService } from './sprint/sprint.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -57,7 +58,8 @@ import { SearchSelectComponent } from "./search-select/search-select.component";
   providers: [
     {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-      useValue: { appearance: "fill" }
+      useValue: { appearance: "fill" },
+      providers: [SprintService]
     }
   ],
   bootstrap: [AppComponent],
--- src/app/sprint/sprint.service.ts
+++ src/app/sprint/sprint.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class SprintService {
+  subject = new Subject<string>();
+  constructor() {}
 
-  constructor() { }
+  getSprintSubject(): Subject<string> {
+    return this.subject;
+  }
 
+  setSubjectWithInitialLoad() {
+    this.subject.next("First");
+  }
+
+  putNewSprintInList(varx: string) {
+    this.subject.next(varx);
+  }
 }
